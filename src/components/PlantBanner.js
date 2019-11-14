79cca4c10d73166d5bdcfa0c281859d0bf77c3cb
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-11-14 10:03:50 +0000

    added scroll feature to image cards

---
+++ src/components/PlantBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const plantBanner = () => {
+    return (
+        <div className="Green-rectangle">
+            <img className="cta-tree-right" src={require('../img/Illustrations/CTA/PNG/cta-tree-right.png')} alt=></img>
+            <h2>Plant a Tree</h2>
+            <h3>Reduce your carbon input </h3>
+            <button>Plant a tree</button>
+        </div>
+    )
+}
