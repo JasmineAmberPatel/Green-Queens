be0844fc377d18bd68c4971a01e859e6cf4d1ee5
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-11-20 12:07:03 +0000

    edited styling for the plant a tree section

--- src/components/PlantBanner.js
+++ src/components/PlantBanner.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import '../styles/TreeBanner.css';
-import { Route } from 'react-router';
-import PlantTree from './PlantTree';
-import { BrowserRouter } from 'react-router-dom';
 
 const plantBanner = () => {
     return (
